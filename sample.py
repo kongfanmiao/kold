4c7b64a07e317f8f0409499a11768130c557435a
Fanmiao Kong <fanmiaokong1223@gmail.com>
2021-03-05 22:10:58 +0000

    Update sample.py
    First version of Sample, Chip and Device. Allow create, read devices_log.json file or append new device to it in appropriate time. All of these is done automatically when these objects are created.

--- sample.py
+++ sample.py
@@ -1,11 +1,131 @@
+import os
+import json
+from configuration.config import config_base
 
-from .configuration.config import config_base
+from qcodes.utils.helpers import  named_repr
+
+from typing import  Union
 
 
-class Sample():
 
-    def __init__(self, name, path):
+class Sth():
+
+    def __init__(self, create_dir=True):
+        self.path = config_base['root_dir']
+        
+
+    def create_directory(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+            msg = "{} directory {} created".format(self.__class__.__name__,
+                                                    self.path)
+            print(msg)
+    
+    def __repr__(self):
+        return named_repr(self)
+
+
+
+class Sample(Sth):
+
+    def __init__(self, name, create_dir=True):
+        super().__init__()
         self.name = name
-        self.path = path
+        self.path = self.get_path()
+        if create_dir:
+            self.create_directory()
+        self.chips = set()
+        self.devices = {}
 
-print(config_base['root_dir'])
+        self.log_path = os.path.join(self.path, "devices_log.json")
+        try:
+            with open(self.log_path) as fp:
+                dvs = json.load(fp)
+                self.chips = set(dvs.keys())
+                self.devices = dvs
+        except FileNotFoundError:
+            self.log_devices()
+
+    def get_path(self):
+        return os.path.join(config_base['root_dir'], self.name)
+
+    def log_devices(self):
+        with open(self.log_path, 'w') as fp:
+            dvs_new = {}
+            for ch, dvs in self.devices.items():
+                dvs_new[ch] = list(dvs)
+            json.dump(dvs_new, fp, indent=4)
+        msg = "Devices logged"
+        print(msg)
+
+
+
+class Device(Sth):
+
+    def __init__(self, name, sample, chip, create_dir=True):
+        super().__init__()
+        self.name = name
+        if isinstance(sample, Sample):
+            self.sample = sample
+        elif isinstance(sample, str):
+            self.sample = Sample(name=sample)
+        if isinstance(chip, Chip):
+            self.chip = chip
+        elif isinstance(chip, str):
+            self.chip = Chip(name=chip, sample=self.sample)
+        if self.name not in self.chip.devices:
+            self.chip.add_device(self)
+
+        self.path = self.get_path()
+        if create_dir:
+            self.create_directory()
+    
+    def get_path(self):
+        return os.path.join(config_base['root_dir'],
+                            self.sample.name,
+                            self.chip.name,
+                            self.name)
+    
+
+
+class Chip(Sth):
+    
+    def __init__(self, name, sample, create_dir=True):
+        super().__init__()
+        self.name = name
+        if isinstance(sample, Sample):
+            self.sample = sample
+        elif isinstance(sample, str):
+            self.sample = Sample(name=sample)
+        self.sample = sample
+        self.path = self.get_path()
+        if self.name in self.sample.chips:
+            self.devices = self.sample.devices[self.name]
+        else:
+            self.devices = set()
+            self.sample.chips.add(self.name)
+            self.sample.devices[self.name] = self.devices
+            self.sample.log_devices()
+
+        if create_dir:
+            self.create_directory()
+
+
+    def get_path(self):
+        return os.path.join(config_base['root_dir'],
+                            self.sample.name,
+                            self.name)
+
+    
+    def add_device(self, device):
+        if isinstance(device, Device):
+            self.devices.add(device.name)
+        elif isinstance(device, str):
+            self.devices.add(device)
+        self.sample.devices[self.name] = self.devices
+        self.sample.log_devices()
+
+
+
+
+
